467b576adcc469987773c39d80745e13d719dd53
MUHAMMAD KHIZAR RIAZ <58303763+khizar7422@users.noreply.github.com>
2023-07-18 11:25:35 +0200

    stateless function component and stateful class component

---
+++ src/component/HelloClass.js
@@ -0,0 +1,15 @@
+import React, { Component } from "react";
+
+
+class HelloClass extends Component {
+    render()
+    {
+        return(
+            <>
+            <h3>hello from stateful class component</h3>
+            </>
+        );
+    }
+}
+
+export default HelloClass;
---
+++ src/component/Hellofunction.js
@@ -0,0 +1,10 @@
+import React from "react";
+
+function HelloFunction(){
+    return(
+        <div>
+            <p>hello from stateless function component</p>
+        </div>
+    );
+}
+export default HelloFunction;
