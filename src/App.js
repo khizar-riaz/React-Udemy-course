e04c159d1b3688f20ef2eecc12ae0113d2da3ece
MUHAMMAD KHIZAR RIAZ <58303763+khizar7422@users.noreply.github.com>
2023-07-18 15:15:43 +0200

    es6 functional component

--- src/App.js
+++ src/App.js
@@ -1,6 +1,7 @@
 import './App.css';
 import HelloClass from './component/HelloClass';
 import HelloFunction from './component/Hellofunction';
+import HelloFunctionES6 from './component/HellofunctionES6';
 
 function App() {
   return (
@@ -8,6 +9,7 @@ function App() {
  <h2>Hello App component</h2>
 <HelloFunction/>
 <HelloClass/>
+<HelloFunctionES6/>
     </div>
   );
 }
---
+++ src/component/HellofunctionES6.js
@@ -0,0 +1,4 @@
+import React from "react";
+
+const HelloFunctionES6 = () => <h3>hello from stateless function ES6 component</h3>
+export default HelloFunctionES6;
