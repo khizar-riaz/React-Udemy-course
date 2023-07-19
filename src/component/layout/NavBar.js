9496dfb177dddfdaf7afcdc86fe56b25643e692d
MUHAMMAD KHIZAR RIAZ <58303763+khizar7422@users.noreply.github.com>
2023-07-19 15:04:57 +0200

    Routing the pages without the browser reload

--- src/component/layout/NavBar.js
+++ src/component/layout/NavBar.js
@@ -1,12 +1,12 @@
 import React from "react";
-
+import { Link, NavLink } from "react-router-dom";
 function NavBar() {
   return (
     <nav className="navbar navbar-expand-lg  bg-info">
       <div className="container-fluid">
-        <a className="navbar-brand" href="/">
+        <NavLink className="navbar-brand" exact to="/">
           UMSA
-        </a>
+        </NavLink>
         <button
           className="navbar-toggler"
           type="button"
@@ -21,19 +21,19 @@ function NavBar() {
         <div className="collapse navbar-collapse" id="navbarSupportedContent">
           <ul className="navbar-nav me-auto mb-2 mb-lg-0">
             <li className="nav-item">
-              <a className="nav-link active" aria-current="page" href="/">
+              <NavLink className="nav-link" aria-current="page" exact to="/">
                 Home
-              </a>
+              </NavLink>
             </li>
             <li className="nav-item">
-              <a className="nav-link" href="/about">
+              <NavLink className="nav-link" exact to="/about">
                 About
-              </a>
+              </NavLink>
             </li>
             <li className="nav-item">
-              <a className="nav-link" href="/contact">
+              <NavLink className="nav-link" exact to="/contact">
                 Contact
-              </a>
+              </NavLink>
             </li>
           </ul>
         </div>
