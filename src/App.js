34972e131a72e9d526a7f8817da461c75f012b64
MUHAMMAD KHIZAR RIAZ <58303763+khizar7422@users.noreply.github.com>
2023-07-19 14:33:40 +0200

    Navbar

--- src/App.js
+++ src/App.js
@@ -3,9 +3,11 @@ import '../node_modules/bootstrap/dist/css/bootstrap.css'
 import HomePage from './component/pages/HomePage';
 import AboutPage from './component/pages/AboutPage';
 import ContactPage from './component/pages/ContactPage';
+import NavBar from './component/layout/NavBar';
 function App() {
   return (
     <div className="container">
+      <NavBar/>
     <HomePage/>
     <AboutPage/>
     <ContactPage/>
