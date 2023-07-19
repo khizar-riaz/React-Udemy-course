4792d1d4f33e5d20ec3ac9a260117bd206098545
MUHAMMAD KHIZAR RIAZ <58303763+khizar7422@users.noreply.github.com>
2023-07-19 12:37:06 +0200

    updated set state

--- src/component/Count.js
+++ src/component/Count.js
@@ -1,29 +1,30 @@
-import React, { Component } from 'react'
+import React, { Component } from "react";
 
- class Count extends Component {
-     constructor(props) {
-       super(props)
-     
-       this.state = {
-          c : 0
-       }
-       }
-       increase(){
-        this.setState({
-      c: this.state.c+1
-        });
+class Count extends Component {
+  constructor(props) {
+    super(props);
+
+    this.state = {
+      c: 0,
+    };
+  }
+  increase() {
+    this.setState(
+      {
+        c: this.state.c + 1,
+      },
+      () => {
         console.log(this.state.c);
-       
-     }
-
-
+      }
+    );
+  }
 
   render() {
     return (
-      <div>Count component: {this.state.c}
-       <button onClick={()=>this.increase()}> Increase</button>
+      <div>
+        Count component: {this.state.c}
+        <button onClick={() => this.increase()}> Increase</button>
       </div>
-      
     );
   }
 }
