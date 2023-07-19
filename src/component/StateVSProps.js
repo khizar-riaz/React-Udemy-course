49ac6bd2fe578c70eb71c8549ce60f46cf4884e2
MUHAMMAD KHIZAR RIAZ <58303763+khizar7422@users.noreply.github.com>
2023-07-19 11:55:07 +0200

    change state of object  in a class

--- src/component/StateVSProps.js
+++ src/component/StateVSProps.js
@@ -5,9 +5,20 @@ export class StateVSProps extends Component {
         super(); //calling the super class constructor
         this.state = { Name: "khizar", age: 24};
     }
+    changeData() {
+        //debugger;
+        this.setState({
+         Name: "Ali",
+         age: 21
+        });
+   
+    }
   render() {
     return (
-      <div>hello my name is {this.state.Name} and age is {this.state.age}</div>
+      <div>
+        <h2>hello my name is {this.state.Name} and age is {this.state.age}</h2>
+        <button onClick={()=>this.changeData()}>Change Details</button>
+      </div>
     );
   }
 }
