64dab020d5e4d008426203ba043097e5d0373eb7
MUHAMMAD KHIZAR RIAZ <58303763+khizar7422@users.noreply.github.com>
2023-07-19 12:34:50 +0200

    set state

--- src/App.js
+++ src/App.js
@@ -1,15 +1,12 @@
 import './App.css';
 import HelloClass from './component/HelloClass';
-import HelloFunction from './component/Hellofunction';
-import HelloFunctionES6 from './component/HellofunctionES6';
-import StateVSProps from './component/StateVSProps';
-import Propsfunction from './component/propsfunction';
+import Count from './component/Count';
 function App() {
   return (
     <div className="App">
  <h2>Hello App component</h2>
 
-<StateVSProps/>
+<Count/>
 
     </div>
   );
