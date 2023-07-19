f0ff3de6c4a442220af55d7df3ce6caff0d9f114
MUHAMMAD KHIZAR RIAZ <58303763+khizar7422@users.noreply.github.com>
2023-07-19 14:47:05 +0200

    Routing the pages

--- src/App.js
+++ src/App.js
@@ -1,17 +1,22 @@
-import './App.css';
-import '../node_modules/bootstrap/dist/css/bootstrap.css'
-import HomePage from './component/pages/HomePage';
-import AboutPage from './component/pages/AboutPage';
-import ContactPage from './component/pages/ContactPage';
-import NavBar from './component/layout/NavBar';
+import "./App.css";
+import "../node_modules/bootstrap/dist/css/bootstrap.css";
+import HomePage from "./component/pages/HomePage";
+import AboutPage from "./component/pages/AboutPage";
+import ContactPage from "./component/pages/ContactPage";
+import NavBar from "./component/layout/NavBar";
+import { BrowserRouter as Router, Route, Routes } from "react-router-dom";
 function App() {
   return (
-    <div className="container">
-      <NavBar/>
-    <HomePage/>
-    <AboutPage/>
-    <ContactPage/>
-    </div>
+    <Router>
+      <div>
+        <NavBar />
+        <Routes>
+          <Route exact path="/" element={<HomePage />} />
+          <Route exact path="/about" element={<AboutPage />} />
+          <Route exact path="/contact" element={<ContactPage />} />
+        </Routes>
+      </div>
+    </Router>
   );
 }
 
--- src/component/layout/NavBar.js
+++ src/component/layout/NavBar.js
@@ -1,30 +1,45 @@
-import React from 'react'
+import React from "react";
 
 function NavBar() {
   return (
     <nav className="navbar navbar-expand-lg  bg-info">
-    <div className="container-fluid">
-      <a className="navbar-brand" href="/">UMSA</a>
-      <button className="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
-        <span className="navbar-toggler-icon"></span>
-      </button>
-      <div className="collapse navbar-collapse" id="navbarSupportedContent">
-        <ul className="navbar-nav me-auto mb-2 mb-lg-0">
-          <li className="nav-item">
-            <a className="nav-link active" aria-current="page" href="/">Home</a>
-          </li>
-          <li className="nav-item">
-            <a className="nav-link" href="/about">About</a>
-          </li>
-          <li className="nav-item">
-            <a className="nav-link" href="/contact">Contact</a>
-          </li>
-        </ul>
- 
+      <div className="container-fluid">
+        <a className="navbar-brand" href="/">
+          UMSA
+        </a>
+        <button
+          className="navbar-toggler"
+          type="button"
+          data-bs-toggle="collapse"
+          data-bs-target="#navbarSupportedContent"
+          aria-controls="navbarSupportedContent"
+          aria-expanded="false"
+          aria-label="Toggle navigation"
+        >
+          <span className="navbar-toggler-icon"></span>
+        </button>
+        <div className="collapse navbar-collapse" id="navbarSupportedContent">
+          <ul className="navbar-nav me-auto mb-2 mb-lg-0">
+            <li className="nav-item">
+              <a className="nav-link active" aria-current="page" href="/">
+                Home
+              </a>
+            </li>
+            <li className="nav-item">
+              <a className="nav-link" href="/about">
+                About
+              </a>
+            </li>
+            <li className="nav-item">
+              <a className="nav-link" href="/contact">
+                Contact
+              </a>
+            </li>
+          </ul>
+        </div>
       </div>
-    </div>
-  </nav>
+    </nav>
   );
 }
 
-export default NavBar
+export default NavBar;
