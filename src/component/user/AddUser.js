b0627093cf0d37e952f2d7b4ba3327c63e5b2b74
MUHAMMAD KHIZAR RIAZ <58303763+khizar7422@users.noreply.github.com>
2023-07-21 16:32:07 +0200

    save new user to database via post api call

--- src/component/user/AddUser.js
+++ src/component/user/AddUser.js
@@ -1,23 +1,55 @@
 import React, { useState } from "react";
-
+import axios from "axios";
+import { useNavigate } from 'react-router-dom'
 
 const AddUser = () => {
+  const navigate = useNavigate();
+  const initialState = {
+    name: "",
+    username: "",
+    email: "",
+    phone: "",
+    website: "",
+  };
+  const [user, setUser] = useState(initialState);
 
-const initialState = { name: "", username: "", email: "", phone: "", website: ""};
-const [ user, setUser] = useState(initialState)
-
-const { name, username, email, phone, website} = user; //object destructing
+  const { name, username, email, phone, website } = user; //object destructing
 
   const onChangeInput = (event) => {
     console.log(event);
-    setUser({...user, [event.target.name]: event.target.value});
+    //spread operators appends to existing data
+    setUser({ ...user, [event.target.name]: event.target.value });
+  }
+
+  const onformSubmit = async (event) => {
+    event.preventDefault();   //to prevent the reloading of the page
+    if(!user.name){
+      alert("Name Cannot be empty")
+      return;
+    }
+    if(!user.username){
+      alert("Username Cannot be empty")
+      return;
+    }
+    if(!user.email){
+      alert("Email Cannot be empty")
+      return;
+    }
+    if(!user.phone){
+      alert("Phone Cannot be empty")
+      return;
+    }
+
+  await axios.post ("http://localhost:5000/users", user);
+  navigate({pathname: '/'})
+
   }
 
   return (
     <div className="container">
       <div className="w-75 mx-auto p-5 shadow-sm">
         <h2 className="text-center mb-4">Add User</h2>
-        <form>
+        <form onSubmit={(event) =>onformSubmit(event)}>
           <div className="form-group mb-2">
             <input
               type="text"
@@ -68,7 +100,7 @@ const { name, username, email, phone, website} = user; //object destructing
               onChange={(event) => onChangeInput(event)}
             />
           </div>
-          <button className="btn btn-info text-white col-12">Add User</button>
+          <button type="submit" className="btn btn-info text-white col-12">Add User</button>
         </form>
       </div>
     </div>
