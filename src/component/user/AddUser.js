00d6c2671918141c43e1ff1fc18a592f5596292c
MUHAMMAD KHIZAR RIAZ <58303763+khizar7422@users.noreply.github.com>
2023-07-21 16:02:22 +0200

    set state variable data

--- src/component/user/AddUser.js
+++ src/component/user/AddUser.js
@@ -1,72 +1,78 @@
-import React from 'react'
+import React, { useState } from "react";
+
 
 const AddUser = () => {
 
-   const onChangeInput = event => {
+const initialState = { name: "", username: "", email: "", phone: "", website: ""};
+const [ user, setUser] = useState(initialState)
 
-   }
+const { name, username, email, phone, website} = user; //object destructing
+
+  const onChangeInput = (event) => {
+    console.log(event);
+    setUser({...user, [event.target.name]: event.target.value});
+  }
 
   return (
-    <div className='container'>
-        <div className='w-75 mx-auto p-5 shadow-sm'>
-          <h2 className='text-center mb-4'>Add User</h2>
-          <form>
-            <div className='form-group mb-2'>
-              <input type='text'
-               className='form-control from-control-lg'
-               placeholder='Enter your full name'
-               name='fullname'
-               value=""
-               onChange={(event)=>onChangeInput(event)}
-
-              />  
-              </div>
-           <div className='form-group mb-2'>
-              <input type='text'
-               className='form-control from-control-lg'
-               placeholder='Enter your username'
-               name='username'
-               value=""
-               onChange={(event)=>onChangeInput(event)}
-
-              />  
-            </div>
-            <div className='form-group mb-2'>
-              <input type='email'
-               className='form-control from-control-lg'
-               placeholder='Enter your email'
-               name='email'
-               value=""
-               onChange={(event)=>onChangeInput(event)}
-
-              />  
-            </div>
-            <div className='form-group mb-2'>
-              <input type='text'
-               className='form-control from-control-lg'
-               placeholder='Enter your phone number'
-               name='phone'
-               value=""
-               onChange={(event)=>onChangeInput(event)}
-
-              />  
-            </div>
-            <div className='form-group mb-2'>
-              <input type='text'
-               className='form-control from-control-lg'
-               placeholder='Enter your website'
-               name='website'
-               value=""
-               onChange={(event)=>onChangeInput(event)}
-
-              />  
-            </div>
-            <button className='btn btn-info text-white col-12'>Add User</button>
-          </form>
-        </div>
-
+    <div className="container">
+      <div className="w-75 mx-auto p-5 shadow-sm">
+        <h2 className="text-center mb-4">Add User</h2>
+        <form>
+          <div className="form-group mb-2">
+            <input
+              type="text"
+              className="form-control from-control-lg"
+              placeholder="Enter your full name"
+              name="name"
+              value={name}
+              onChange={(event) => onChangeInput(event)}
+            />
+          </div>
+          <div className="form-group mb-2">
+            <input
+              type="text"
+              className="form-control from-control-lg"
+              placeholder="Enter your username"
+              name="username"
+              value={username}
+              onChange={(event) => onChangeInput(event)}
+            />
+          </div>
+          <div className="form-group mb-2">
+            <input
+              type="email"
+              className="form-control from-control-lg"
+              placeholder="Enter your email"
+              name="email"
+              value={email}
+              onChange={(event) => onChangeInput(event)}
+            />
+          </div>
+          <div className="form-group mb-2">
+            <input
+              type="text"
+              className="form-control from-control-lg"
+              placeholder="Enter your phone number"
+              name="phone"
+              value={phone}
+              onChange={(event) => onChangeInput(event)}
+            />
+          </div>
+          <div className="form-group mb-2">
+            <input
+              type="text"
+              className="form-control from-control-lg"
+              placeholder="Enter your website"
+              name="website"
+              value={website}
+              onChange={(event) => onChangeInput(event)}
+            />
+          </div>
+          <button className="btn btn-info text-white col-12">Add User</button>
+        </form>
+      </div>
     </div>
   );
-}
+};
 
-export default AddUser
+export default AddUser;
