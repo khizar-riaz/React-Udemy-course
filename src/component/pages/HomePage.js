97e90b3e526834dd6924cf2eba7f54d356b9453b
MUHAMMAD KHIZAR RIAZ <58303763+khizar7422@users.noreply.github.com>
2023-07-21 11:26:57 +0200

    fatching users data from axios

--- src/component/pages/HomePage.js
+++ src/component/pages/HomePage.js
@@ -1,6 +1,24 @@
-import React from 'react'
+import React, { useEffect, useState } from 'react'
+import axios from 'axios';
 
 function HomePage() {
+
+  const [users, setusers] = useState([]); //we are creating a users array and its setter method and its an empty array
+   
+  useEffect(()=>{
+    getAllUsers();
+  }, []); //empty array to load content once
+
+  const getAllUsers = () =>{
+    axios.get("http://localhost:5000/users")
+    .then(function(response)
+    {
+      console.log("a response",response);
+    }) //do something if the data is feteched successfully
+    .catch(function(error){
+      console.log("an error", error);
+    }); //do something if there is an error
+  }
   return (
     <div>HomePage</div>
   );
