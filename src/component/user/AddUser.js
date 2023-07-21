093869618a527f5fe5922b66c5c8e77f588af63b
MUHAMMAD KHIZAR RIAZ <58303763+khizar7422@users.noreply.github.com>
2023-07-21 15:09:23 +0200

    create Add User Form

--- src/component/user/AddUser.js
+++ src/component/user/AddUser.js
@@ -1,8 +1,71 @@
 import React from 'react'
 
 const AddUser = () => {
+
+   const onChangeInput = event => {
+
+   }
+
   return (
-    <div>AddUser</div>
+    <div className='container'>
+        <div className='w-75 mx-auto p-5 shadow-sm'>
+          <h2 className='text-center mb-4'>Add User</h2>
+          <form>
+            <div className='form-group mb-2'>
+              <input type='text'
+               className='form-control from-control-lg'
+               placeholder='Enter your full name'
+               name='fullname'
+               value=""
+               onChange={(event)=>onChangeInput(event)}
+
+              />  
+              </div>
+           <div className='form-group mb-2'>
+              <input type='text'
+               className='form-control from-control-lg'
+               placeholder='Enter your username'
+               name='username'
+               value=""
+               onChange={(event)=>onChangeInput(event)}
+
+              />  
+            </div>
+            <div className='form-group mb-2'>
+              <input type='email'
+               className='form-control from-control-lg'
+               placeholder='Enter your email'
+               name='email'
+               value=""
+               onChange={(event)=>onChangeInput(event)}
+
+              />  
+            </div>
+            <div className='form-group mb-2'>
+              <input type='text'
+               className='form-control from-control-lg'
+               placeholder='Enter your phone number'
+               name='phone'
+               value=""
+               onChange={(event)=>onChangeInput(event)}
+
+              />  
+            </div>
+            <div className='form-group mb-2'>
+              <input type='text'
+               className='form-control from-control-lg'
+               placeholder='Enter your website'
+               name='website'
+               value=""
+               onChange={(event)=>onChangeInput(event)}
+
+              />  
+            </div>
+            <button className='btn btn-info text-white col-12'>Add User</button>
+          </form>
+        </div>
+
+    </div>
   );
 }
 
