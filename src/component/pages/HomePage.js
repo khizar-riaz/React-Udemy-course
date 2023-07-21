e61dcbe355f555d4dd5cc1d55893ebc460fb833e
MUHAMMAD KHIZAR RIAZ <58303763+khizar7422@users.noreply.github.com>
2023-07-21 12:09:48 +0200

    setusers to save data

--- src/component/pages/HomePage.js
+++ src/component/pages/HomePage.js
@@ -14,6 +14,8 @@ function HomePage() {
   const getAllUsersWithAwait = async () =>{
     const result =  await axios.get("http://localhost:5000/users");
     console.log(result);
+    setusers(result.data);
+
     console.log("after axios call");
   }
   const getAllUsers = () =>{
