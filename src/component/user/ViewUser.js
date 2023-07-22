c4603653ef36b6bb4b974c8c34ce15168149b2f4
MUHAMMAD KHIZAR RIAZ <58303763+khizar7422@users.noreply.github.com>
2023-07-22 14:43:59 +0200

    About and contact page

--- src/component/user/ViewUser.js
+++ src/component/user/ViewUser.js
@@ -1,6 +1,6 @@
 import React, { useState, useEffect } from "react";
 import axios from "axios";
-import { Link , useParams } from "react-router-dom";
+import { Link, useParams } from "react-router-dom";
 import { ListGroup } from "react-bootstrap";
 
 const ViewUser = () => {
@@ -13,8 +13,8 @@ const ViewUser = () => {
     website: "",
   };
   const [user, setUser] = useState(initialState);
-  const [address, setAddress] = useState({})
-  const [company, setCompany] = useState({})
+  const [address, setAddress] = useState({});
+  const [company, setCompany] = useState({});
 
   useEffect(() => {
     fetchUser(); //will call only once on page load as we have pased empty depedency array
@@ -24,20 +24,24 @@ const ViewUser = () => {
     const response = await axios.get(`http://localhost:5000/users/${userId}`);
     //console.log(response);
     setUser(response.data);
-    setAddress(response.data.address)
-    setCompany(response.data.company)
+    setAddress(response.data.address);
+    setCompany(response.data.company);
   };
   return (
     <div className="container">
-        <Link to="/" className="btn btn-outline-info mt-2 ">Back</Link>
-        <p className="display-2">User Id: {user.id}</p>
+      <Link to="/" className="btn btn-outline-info mt-2 ">
+        Back
+      </Link>
+      <p className="display-2">User Id: {user.id}</p>
       <ListGroup variant="flush" className="P=2 W-75">
         <ListGroup.Item>Full Name: {user.name}</ListGroup.Item>
         <ListGroup.Item>Email: {user.email}</ListGroup.Item>
         <ListGroup.Item>Phone: {user.phone}</ListGroup.Item>
         <ListGroup.Item>UserName: {user.username}</ListGroup.Item>
         <ListGroup.Item>Website: {user.website}</ListGroup.Item>
-        <ListGroup.Item>Address: {address.street} | {address.city}</ListGroup.Item>
+        <ListGroup.Item>
+          Address: {address.street} | {address.city}
+        </ListGroup.Item>
         <ListGroup.Item>Company Name: {company.name}</ListGroup.Item>
       </ListGroup>
     </div>
